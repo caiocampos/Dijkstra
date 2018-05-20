f4cdef36ac859d15703f603eb6c2db6d5dd58286
Caio <caio_campos1204@hotmail.com>
2018-05-20 10:21:16 -0300

    Bases do algoritmo

---
+++ src/main/java/main/dijkstra/model/Graph.java
@@ -0,0 +1,25 @@
+package main.dijkstra.model;
+
+import java.util.List;
+
+/**
+ * Grafo com seus nós e caminhos
+ */
+public class Graph {
+
+	private final List<Vertex> vertexes;
+	private final List<Edge> edges;
+
+	public Graph(List<Vertex> vertexes, List<Edge> edges) {
+		this.vertexes = vertexes;
+		this.edges = edges;
+	}
+
+	public List<Vertex> getVertexes() {
+		return vertexes;
+	}
+
+	public List<Edge> getEdges() {
+		return edges;
+	}
+}
